38e55a8ca4b6933e560a679950f5bf1acf0eae81
Douglas Deodato <douglasdeodato@gmail.com>
2015-08-15 23:18:01 +0100

    grunt dist

---
+++ public/myapp.production.js
@@ -0,0 +1,2 @@
+var data=require("./test");console.log(data.name),console.log(data.name),console.log(data),foo=1,module.exports={name:"douglas deodato"};
+//# sourceMappingURL=myapp.production.js.map
